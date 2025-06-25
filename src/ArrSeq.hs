3d2416c7a5fb5c6d7248776d5acbb2823ea33fb0
Bussita <santiagobussanich321@gmail.com>
2025-06-24 23:35:20 -0300

    changes on reduce function

--- src/ArrSeq.hs
+++ src/ArrSeq.hs
@@ -81,28 +81,3 @@ instance Seq A.Arr where
     | otherwise = e `op` v
     where
       v = reduceT op (toTree s)
-
-data Tree a = Leaf a | Node (Tree a) (Tree a)
-
-toTree :: A.Arr a -> Tree a
-toTree s
-  | lengthS s == 1 = Leaf (nthS s 0)
-  | otherwise =
-      let (l', r') = (toTree (takeS s pp)) ||| (toTree (dropS s pp))
-       in Node l' r'
-  where
-    pp = 2 ^ ilog2 ((lengthS s) - 1)
-
-reduceT :: (a -> a -> a) -> Tree a -> a
-reduceT op (Leaf x) = x
-reduceT op (Node l r) = l' `op` r'
-  where
-    (l', r') = (reduceT op l) ||| (reduceT op r)
-
-ilog2 :: Int -> Int
-ilog2 n
-  | n < 1 = error "ilog2 no definido para n < 1"
-  | otherwise = go n 0
-  where
-    go 1 acumulador = acumulador
-    go x acumulador = go (x `div` 2) (acumulador + 1)
--- src/ListSeq.hs
+++ src/ListSeq.hs
@@ -1,11 +1,8 @@
 module ListSeq where
 
-import Data.Foldable (Foldable (foldl'))
 import GHC.Float
 import Par
 import Seq
-import Seq (TreeView (EMPTY))
-import Data.List (singleton)
 
 instance Seq [] where
   emptyS = []
@@ -19,9 +16,9 @@ instance Seq [] where
   tabulateS f n = [f i | i <- [0 .. n - 1]]
 
   mapS _ [] = []
-  mapS f (x : xs) = appendS (singletonS x') rest
+  mapS f (x : xs) = x' : rest
     where
-      (x',rest) = f x ||| mapS f xs 
+      (x', rest) = f x ||| mapS f xs
 
   filterS = filter
 
@@ -45,39 +42,28 @@ instance Seq [] where
         let (v, rest) = nthS s 0 ||| dropS s 1
          in CONS v rest
 
-  joinS = reduceS appendS emptyS
+  joinS = reduceS appendS emptyS -- ToDo : revisar
 
   reduceS op e s
     | lengthS s == 0 = e
-    | otherwise = e `op` v
-    where
-      v = reduceT op (toTree s)
+    | lengthS s == 1 = nthS s 0
+    | otherwise =
+        let m = div (lengthS s) 2
+            (l', r') = takeS s m ||| dropS s m
+         in reduceS op e l' `op` reduceS op e r'
 
-  scanS op e s = (tabulateS (\i -> reduceS op e (takeS s i)) (lengthS s), reduceS op e s)
+  scanS op e s
+    | lengthS s == 0 = ([], e)
+    | lengthS s == 1 = ([e], e `op` (s !! 0))
+    | otherwise =
+        let cont :: [a]
+            cont = [ (s !! (2*i)) `op` (s !! (2*i+1)) | i <- [0 .. (length s `div` 2) - 1] ]
+            (s', red) = scanS op e cont
+            r :: [a]
+            r = [ if even i
+                    then s' !! (i `div` 2)
+                    else (s' !! (i `div` 2)) `op` (s !! (i-1))
+                | i <- [0 .. length s - 1] ]
+        in (r, red)
 
   fromList = id
-
-data Tree a = Leaf a | Node (Tree a) (Tree a)
-
-toTree :: [a] -> Tree a
-toTree s
-  | lengthS s == 1 = Leaf (nthS s 0)
-  | otherwise =
-      let (l', r') = toTree (takeS s pp) ||| toTree (dropS s pp)
-       in Node l' r'
-  where
-    pp = 2 ^ ilog2 (lengthS s - 1)
-
-reduceT :: (a -> a -> a) -> Tree a -> a
-reduceT op (Leaf x) = x
-reduceT op (Node l r) = l' `op` r'
-  where
-    (l', r') = reduceT op l ||| reduceT op r
-
-ilog2 :: Int -> Int
-ilog2 n
-  | n < 1 = error "ilog2 no definido para n < 1"
-  | otherwise = go n 0
-  where
-    go 1 acumulador = acumulador
-    go x acumulador = go (x `div` 2) (acumulador + 1)
