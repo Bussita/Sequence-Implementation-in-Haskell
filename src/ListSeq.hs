a160bab862757282698bfd4d6f6a2a396a77974e
ernestosavio <ernesto_savio@hotmail.com>
2025-06-26 17:17:51 -0300

    reimplementacion ListSeq terminada y testeada

--- src/ListSeq.hs
+++ src/ListSeq.hs
@@ -10,31 +10,45 @@ instance Seq [] where
 
   singletonS x = [x]
 
-  lengthS empty = 0
+  lengthS [] = 0
   lengthS (x:xs) = 1 + lengthS xs 
 
-  nthS empty _ = error "Index out of range."  
+  nthS [] _ = error "Index out of range."  
   nthS (x:xs) n = if n == 0 then x else nthS xs (n-1)
 
-  tabulateS f n = [f i | i <- [0 .. n - 1]]
+  tabulateS f 0 = [f 0]
+  tabulateS f n = tabulateS' f (n - 1) 0
+    where
+    tabulateS' :: (Int -> a) -> Int -> Int -> [a]
+    tabulateS' f n i
+            | i == n = [f n]
+            | otherwise =
+                          let
+                            (fx, rest) = f i ||| tabulateS' f n (i + 1)
+                          in fx : rest
 
   mapS _ [] = []
   mapS f (x : xs) = x' : rest
     where
       (x', rest) = f x ||| mapS f xs
 
-  filterS p empty = empty
-  filterS p xs = [x | p x, x <- xs] 
+  filterS p [] = emptyS 
+  filterS p (x : xs) = 
+                       let
+                         (res, rest) = p x ||| filterS p xs
+                       in 
+                         if res then x : rest else rest
+          
 
-  appendS empty t = t
+  appendS [] t = t
   appendS (x:xs) t = x : appendS xs t 
 
-  takeS s 0 = empty
+  takeS s 0 = emptyS 
   takeS (x:xs) n = x : takeS xs (n-1)
 
   dropS s 0 = s
-  dropS empty _ = empty
-  dropS (x:xs) n = dropS xs (n-1) 
+  dropS [] _ = emptyS 
+  dropS (x : xs) n = dropS xs (n - 1) 
 
   showtS s
     | lengthS s == 0 = EMPTY
@@ -50,38 +64,94 @@ instance Seq [] where
         let (v, rest) = nthS s 0 ||| dropS s 1
          in CONS v rest
 
-  -- joinS = reduceS appendS emptyS -- ToDo : revisar
+  joinS [] = emptyS
+  joinS (s:ss) = nthS s 0 : joinS (dropS s 1 : ss) 
 
-  joinS empty = empty
-  joinS (s:ss) = nthS s 0 : joinS (dropS 1 s : ss) 
-
--- Arreglar, hacer particion PP
   reduceS op e s
     | lengthS s == 0 = e
-    | lengthS s == 1 = nthS s 0
-    | otherwise =
-      e
-      where
-          ilog2 :: Int -> Int
-          ilog2 n
-            | n < 1 = error "ilog2 no definido para n < 1"
-            | otherwise = go n 0
-            where
-              go 1 acumulador = acumulador
-              go x acumulador = go (x `div` 2) (acumulador + 1)
+    | lengthS s == 1 = e `op` (nthS s 0)
+    | otherwise = 
+        let 
+            stack = []
+            (_, res) = red (mapS mapFunction s) e stack op
+        in res
+
 
   scanS op e s
-    | lengthS s == 0 = ([], e)
-    | lengthS s == 1 = ([e], e `op` (s !! 0))
+    | lengthS s == 0 = (emptyS, e)
+    | lengthS s == 1 = ([e], e `op` (nthS s 0))
     | otherwise =
-        let cont :: [a]
-            cont = [ (s !! (2*i)) `op` (s !! (2*i+1)) | i <- [0 .. (length s `div` 2) - 1] ]
+        let 
+            (cont, sLen) = contract s [] op ||| lengthS s
             (s', red) = scanS op e cont
-            r :: [a]
-            r = [ if even i
-                    then s' !! (i `div` 2)
-                    else (s' !! (i `div` 2)) `op` (s !! (i-1))
-                | i <- [0 .. length s - 1] ]
+            r = expand s s' 0 sLen op
         in (r, red)
 
+
   fromList = id
+
+
+contract :: [a] -> [a] -> (a -> a -> a) -> [a]
+contract [] ys _ = ys
+contract (x : xs) [] op = contract xs [x] op
+contract (x : xs) (y : ys) op =
+      let
+        (res, rest) = y `op` x ||| contract xs ys op
+      in 
+        res : rest
+
+expand :: [a] -> [a] -> Int -> Int -> (a -> a -> a) -> [a]
+expand s [] _ _ _ = [] -- s' se vacia primero o al mismo tiempo que s', pues tengo length s / 2 techo de elementos
+expand st@(x : s) st'@(y : s') i n op
+    | i == n = []  -- No quiero operar el ultimo de s, en cambio,
+                   -- si el ultimo de s'
+    | (i `mod` 2) == 0 = y : expand st st' (i + 1) n op
+    | otherwise =
+          let
+            (res, rest) = y `op` x ||| expand (dropS s 1)  s' (i + 1) n op
+          in  
+            res : rest
+                  
+red :: [(Int, a)] -> a -> [(Int, a)] -> (a -> a -> a) -> (Int, a)
+red s' e' stack op'
+      | lengthS s' == 0 = foldr (combine op') (0, e') stack
+      | lengthS s' == 1 = 
+        let
+          fst = nthS s' 0
+        in red emptyS e' (eval (fst : stack) op') op'
+      | otherwise = 
+        let 
+            ((_, fst), (_, snd)) = nthS s' 0 ||| nthS s' 1
+            res = (2, fst `op'` snd)
+
+        in case stack of
+             [] -> red (dropS s' 2) e' [res] op'
+             xs -> 
+                   let 
+                     evalStack = eval (res : xs) op'
+                   in red (dropS s' 2) e' evalStack op'
+
+eval :: [(Int, a)] -> (a -> a -> a) -> [(Int, a)]
+eval [] _ = []
+eval (x : []) _ = [x]
+eval stack@((n1, opRes1) : (n2, opRes2) : xs) op
+  | n1 == n2 = eval ((combine op (n1, opRes1) (n2, opRes2)) : xs) op
+  | otherwise = stack
+
+-- Opera el el elemento2 con el elemento 1 -> e2 op e1
+combine :: (a -> a -> a) -> (Int, a) -> (Int, a) -> (Int, a)
+combine op (n1, opRes1) (n2, opRes2) = (n2+n1, opRes2 `op` opRes1)
+
+mapFunction :: a -> (Int, a)
+mapFunction x = (1, x)
+
+ilog2 :: Int -> Int
+ilog2 n
+        | n < 1 = error "ilog2 no definido para n < 1"
+        | otherwise = go n 0
+  where
+    go 1 acumulador = acumulador
+    go x acumulador = go (x `div` 2) (acumulador + 1)
+
+fview :: String -> String -> String
+fview s0 s1 = " (" ++ s0 ++ " + " ++ s1 ++ ") "
